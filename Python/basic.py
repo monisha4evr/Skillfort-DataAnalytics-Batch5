2c4d3c9402ae46bc473b94cb26003ef105704a0e
monisha4evr <monisha.it1510@gmail.com>
2026-09-07 00:05:46 +0530

    Python

---
+++ Python/basic.py
@@ -0,0 +1,54 @@
+# input and output Operation
+# output Operation
+
+# print("Hello Word")
+
+# a_val=50
+# print("a value is : ",a_val)
+
+# print(f"a value is :{a_val} ")
+
+# input("Enter Your Name")
+# print(input("Enter Your Name"))
+
+
+# Below code get username from the user
+name=input("Enter Your Name")
+print(name)
+
+
+Variable
+--------
+
+1. allow alphabets(a-zA-Z),0-9 ,_
+2. cannot start with number 
+3. can start with _(underscore)/Alphabet 
+4. cannot accept space between Variables name
+5. Reserved keyword 
+
+
+firstName 
+
+first_name 
+
+frist name wrong
+
+
+eg: price 
+
+12price  wrong
+
+_price
+
+input  wrong
+
+a=50
+b=10
+a="apple"
+
+print(a+b)
+print(a+b)
+print(a+b)
+print(a+b)
+print(25+20)
+print(24+22)
