3b1de21f4a851b014244d60ac19e67c1754b091e
monisha4evr <monisha.it1510@gmail.com>
2026-09-07 14:13:26 +0530

    data type

--- Python/basic.py
+++ Python/basic.py
@@ -52,3 +52,120 @@ print(a+b)
 print(a+b)
 print(25+20)
 print(24+22)
+
+
+Datatype:
+
+int 
+----
+type() # to find the type of value
+
+a=10 
+print(type(a)) # <class 'int'>
+a=-10 
+print(type(a)) # <class 'int'>
+a=+10 
+print(type(a)) # <class 'int'>
+
+float : 
+------- 
+a=10.5 
+print(type(a)) # <class 'float'>
+
+a=-10.5 
+print(type(a))
+
+str: 
+# sequence of character 
+
+a="flower"
+print(type(a)) # <class 'str'>
+a='flower'
+print(type(a)) # <class 'str'>
+
+bool
+a=True  
+print(type(a)) # <class 'bool'>
+a='true'
+print(type(a)) # <class 'str'>
+
+collection 
+list - [] - hetrogenous , 
+1. ordered ,
+2. mutable ,
+3. allow duplicate 
+eg: 
+    a=[1,5.6,'flower']
+    print(type(a)) # <class 'list'>
+
+
+a=[1,2,3,4,5]
+print(a)
+print(id(a))
+
+tuple
+() ,hetrogenous
+1. ordered
+2. immutable
+3. allow Duplicate
+
+a=(1,2,3,4)
+print(id(a))
+
+set 
+{1,2,3,4}
+- unordered 
+- cannot allow duplicate 
+- mutable
+a={1,2,2,3,4,4,5}
+print(a)
+
+dict 
+key- value 
+        {k:"v"}
+        - key unique 
+        - value allows Duplicate .
+        - not iterable 
+
+
+a={"name":"Apple","color":"Red"}
+print(a)
+
+print(a['name'])
+
+
+immutable :
+
+int 
+str 
+bool 
+None
+float
+tuple 
+
+mutable:
+
+list
+set 
+dict
+
+
+
+immutable : 
+
+a=10
+print(id(a))
+b=10
+print(id(b))
+a=15
+print(id(a))
+print(id(b))
+
+
+a=(1,2,3,4)
+b=(1,2,3,4)
+
+print(id(a))
+print(id(b))
+
+
