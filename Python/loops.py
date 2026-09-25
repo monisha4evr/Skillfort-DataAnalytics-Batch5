55c6547db3cac9b66625a0119be0ce26b88576e0
monisha4evr <monisha.it1510@gmail.com>
2026-09-25 16:55:44 +0530

    loops

--- Python/loops.py
+++ Python/loops.py
@@ -185,9 +185,142 @@ for i in range(1,6):
 for i in range(5,0,-1):
     print("*"*i)
 
- 
- 
+   *
+  **
+ ***
+****
 
+for i in range(1,6):
+    print(" " * (6-i)+ "*"*i)
+ 
+ 
+a= " "
+b= "*"
+print(a+b)
+
+
+*****
+ ****
+  ***
+   **
+    *
+
+for i in range(5,0,-1):
+    print(" "*(5-i) + "*"*i)
+
+Nested Loop 
+
+outer condition -> check condi 
+    inner loop -> loop until condition Fail 
+outer loop -> cond check 
+    inner loop -> loop untill condition fail 
+outer loop -> condition fail 
+
+
+for i in range(1,6):
+    print("Outer",i) 
+    for j in range(1,6):
+      print(j, end=" ")
+    print()
+
+
+
+
+1234
+1234
+1234
+1234
+
+
+execution 
+
+i=[1,2,3]
+j=[1,2,3]
+
+i=1 
+    j=1 2 3
+    i=1 1 1
+i=2
+    j= 1 2 3
+    i= 2 2 2 
+i=3 
+    j= 1 2 3
+    i= 3 3 3 
+
+1111
+2222
+3333
+4444
+
+for i in range(1,5):
+    for j in range(1,5):
+        print(i,end=" ")
+    print()
+
+for i in range(1,4):
+    for j in range(1,4):
+        print(j,end=" ")
+    print()
+
+# Execution:
+# ----------
+i=1
+    j=1   1
+    j=2   2
+    j=3   3
+    j=4   Fail 
+i=2 True 
+    j=1     1
+    j=2 2
+    j=3 3
+    j=4 Fail 
+i=3 
+    j=1     1
+    j=2 2
+    j=3 3
+    j=4 Fail 
+i=4 Fail 
+
+Task:
+-----
+1
+12
+123
+1234
+
+1
+22
+333
+4444
+
+sqr=[]
+for i in range(1,6):
+   sqr.append(i**2)
+print(sqr)
+
+
+list comprehension 
+
+Syntax:
+new_list=[expression for i in iterable]
+
+sqr = [i**2 for i in range(1,6)]
+print(sqr)
+
+a = [1,2,3,4,5]
+print(["Even" if i%2==0 else "Odd" for i in a])
+print([i if i%2==0 else "Odd" for i in a])
+
+
+task : 
+tuplecomplrehension
+dictionarycomprehension
+
+
+
+      
+    
+    
 
 
 
